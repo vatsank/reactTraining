94d3d9a13c2255ade5cbe7d86f40b6e24761e74b
Srivatsan <vatsank@gmail.com>
2019-05-06 14:37:15 +0530

    Spread Example Added

--- es6/es6Functions.js
+++ es6/es6Functions.js
@@ -11,11 +11,52 @@
 
  // Rest Parameters
 
-   function sum(numbers){
-      
+   function sum(a,b,...numbers){
+
+      let total = 0;
+// forEach function takes a arrow function as argument
+    
+console.log(numbers.length);
+
+    numbers.forEach(eachNumber => 
+              total+=eachNumber);
+   
+      console.log(total);
    }
 
  const usd = calculate();
 
  console.log(usd);
+// JavaScript coverts the list of Numbers into an 
+// array since ... rest parameter is used
+ sum(1,2,3,4,5);
 
+
+ // Spread Parameter
+
+   function calculate(a,b,c){
+
+      console.log(a+b+c);
+      console.log(a);
+      console.log(b);
+      console.log(c);
+   }
+
+   let data = [2,4,6];
+
+   calculate(...data);
+
+   const electiveMarks = [70,60,80];
+   const coreMarks = [90,92,98]
+
+   const merge = [electiveMarks,...coreMarks];
+
+   console.log(merge);
+
+   const inital ={id:101,name:'Ramesh'};
+   const updated = {age:45};
+   const markScored =[89,78,90];
+   const mergeObj = {...inital,...updated,
+                   marks:[...markScored]};
+
+   console.log(mergeObj);

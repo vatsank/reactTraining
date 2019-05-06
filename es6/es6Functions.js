2aced8a96af4e6d5313009281e0a5e5fa5651523
Srivatsan <vatsank@gmail.com>
2019-05-06 14:12:16 +0530

    Default Function

---
+++ es6/es6Functions.js
@@ -0,0 +1,21 @@
+// Using Default Value for Parameter
+
+ function calculate(inr=1,convRate=60){
+
+   // if(convRate === undefined){
+   //     convRate = 60;
+   // }
+ 
+    return inr * convRate;
+ }
+
+ // Rest Parameters
+
+   function sum(numbers){
+      
+   }
+
+ const usd = calculate();
+
+ console.log(usd);
+
